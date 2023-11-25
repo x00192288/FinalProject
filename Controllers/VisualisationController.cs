364962397144407367d9ebf10ae6d854fceef3a3
Adam Geraghty <x00192288@mytudublin.ie>
2023-11-25 12:06:44 +0000

    filtering added for visualisation of data

--- Controllers/VisualisationController.cs
+++ Controllers/VisualisationController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 using MySql.Data.MySqlClient;
 
 
@@ -12,42 +14,183 @@ namespace Xampp_Test2.Controllers
 {
     public class VisualisationController : Controller
     {
-        public IActionResult Histogram()
+        public IActionResult Histogram(string timestampYear, string timestampMonth, string timestampDay)
         {
-            List<int> data = new List<int>();
-            MySqlConnection mySqlConnection = new MySqlConnection("server=localhost;database=db_arduino;uid=Arduino;password=x0192288!;SSLMode=none;");
-            string sqlquery = "select temp_value from tbl_temp";
+            string timestamp = "";
 
-            MySqlCommand sqlcomm = new MySqlCommand(sqlquery, mySqlConnection);
-            sqlcomm.CommandTimeout = 60;
-            //MySqlDataReader sdr = sqlcomm.ExecuteReader();
+            // Construct timestamp string based on provided inputs
+            if (!string.IsNullOrEmpty(timestampYear))
+            {
+                timestamp += timestampYear;
+            }
+
+            if (!string.IsNullOrEmpty(timestampMonth))
+            {
+                timestamp += $"-{timestampMonth.PadLeft(2, '0')}";
+            }
+
+            if (!string.IsNullOrEmpty(timestampDay))
+            {
+                timestamp += $"-{timestampDay.PadLeft(2, '0')}";
+            }
+
+            
+            DateTime? filterTimestamp = null;
+            if (DateTime.TryParseExact(timestamp, "yyyy-MM-dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime parsedTimestamp))
+            {
+                filterTimestamp = parsedTimestamp;
+            }
+            // Try parsing with year and month format (yyyy-MM)
+            else if (DateTime.TryParseExact(timestamp, "yyyy-MM", CultureInfo.InvariantCulture, DateTimeStyles.None, out parsedTimestamp))
+            {
+                filterTimestamp = parsedTimestamp;
+            }
+            else if (DateTime.TryParseExact(timestamp, "yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out parsedTimestamp))
+            {
+                filterTimestamp = parsedTimestamp;
+            }
+
+            float[] arduinoArray;
+            float[] apiArray;
 
             try
             {
-                mySqlConnection.Open();
-                MySqlDataReader myReader = sqlcomm.ExecuteReader();
-                if (myReader.HasRows)
-
-                    if (myReader.HasRows)
+                if (filterTimestamp == null)
                 {
-                    Console.WriteLine("Query returned results");
 
-                    while (myReader.Read())
+                    string arduinoSqlQuery = "select temp_value from tbl_temp WHERE temp_value IS NOT NULL";
+                    List<float> arduinoData = SQLQuery("db_arduino", arduinoSqlQuery);
+
+                    if (arduinoData != null && arduinoData.Count > 0)
                     {
-                        int value;
-                        if (int.TryParse(myReader.GetString(0), out value))
-                        {
-                            data.Add(value);
-                        }
+                        arduinoArray = arduinoData.ToArray();
                     }
+                    else
+                    {
+                        arduinoArray = null;
+                    }
+
+                    string apiSqlQuery = "SELECT temperature FROM air_api WHERE temperature IS NOT NULL";
+                    List<float> apiData = SQLQuery("db_air_api", apiSqlQuery);
+
+                   
+
+                    if (apiData != null && apiData.Count > 0)
+                    {
+                        apiArray = apiData.ToArray();
+                    }
+                    else
+                    {
+                        apiArray = null;
+                    }
+
+
+                    if (arduinoData?[0] == 999999 || apiArray?[0] == 999999)
+                    {
+                        return View("Error");
+                    }
+
+
+
                 }
                 else
                 {
-                    Console.WriteLine("Query successfully executed, but no results found");
+                    // Adjust the SQL queries based on the available timestamp components
+                    string arduinoSqlQuery = $"SELECT temp_value FROM tbl_temp WHERE YEAR(timestamp) = {filterTimestamp?.Year}";
+
+                    if (timestampMonth != null)
+                    {
+                        arduinoSqlQuery += $" AND MONTH(timestamp) = {filterTimestamp?.Month}";
+
+                        if (timestampDay != null)
+                        {
+                            arduinoSqlQuery += $" AND DAY(timestamp) = {filterTimestamp?.Day}";
+                        }
+                    }
+
+                    List<float> arduinoData = SQLQuery("db_arduino", arduinoSqlQuery);
+                    arduinoArray = arduinoData.ToArray();
+
+                    string apiSqlQuery = $"SELECT temperature FROM air_api WHERE YEAR(date) = {filterTimestamp?.Year}";
+
+                    if (timestampMonth != null)
+                    {
+                        apiSqlQuery += $" AND MONTH(date) = {filterTimestamp?.Month}";
+
+                        if (timestampDay != null)
+                        {
+                            apiSqlQuery += $" AND DAY(date) = {filterTimestamp?.Day}";
+                        }
+                    }
+
+                    List<float> apiData = SQLQuery("db_air_api", apiSqlQuery);
+                    apiArray = apiData.ToArray();
+
+                    if (arduinoData?[0] == null || apiArray?[0] == null)
+                    {
+                        return View("Error");
+                    }
+                    if (arduinoData?[0] == 999999 || apiArray?[0] == 999999)
+                    {
+                        return View("Error");
+                    }
+                    
                 }
             }
             catch (Exception e)
             {
+                Console.WriteLine("Error: " + e.Message);
+                return View("Error");
+            }
+
+
+
+            return View(new { DataArray = arduinoArray, DataArray2 = apiArray });
+        }
+
+
+
+        public List<float> SQLQuery(string database, string query)
+        {
+            List<float> data = new List<float>();
+
+            MySqlConnection mySqlConnection = new MySqlConnection($"server=localhost;database={database};uid=Arduino;password=x0192288!;SSLMode=none;");
+            string sqlquery = query;
+
+            MySqlCommand sqlcomm = new MySqlCommand(sqlquery, mySqlConnection);
+            sqlcomm.CommandTimeout = 60;
+
+            try
+            {
+                mySqlConnection.Open();
+                using (MySqlDataReader myReader = sqlcomm.ExecuteReader())
+                {
+                    int columnIndex = 0; // Assuming the desired column is the first one (index 0)
+
+                    // Check if the reader has columns
+                    if (myReader.FieldCount > columnIndex)
+                    {
+                        while (myReader.Read())
+                        {
+                            if (!myReader.IsDBNull(columnIndex))
+                            {
+                                float value;
+                                if (float.TryParse(myReader.GetFloat(columnIndex).ToString(), out value))
+                                {
+                                    data.Add(value);
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No columns found in the result set.");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                data.Add(999999);
                 Console.WriteLine("Query error: " + e.Message);
             }
             finally
@@ -55,12 +198,18 @@ namespace Xampp_Test2.Controllers
                 mySqlConnection.Close(); // Close the connection when done
             }
 
-            int[] dataArray = data.ToArray(); // Convert the list to an array
-
-            // Now you can use 'dataArray' which contains the 10 latest values as integers
-
-            return View(dataArray);
+            return data;
         }
+
+
+
+
+
+
+
+
+
+
 
     }
 }
